fc3bbd60d069e9cf01dfe99037c68481e4a61c06
Truong Tien Dat <dd220302@gmail.com>
2025-08-15 13:40:26 +0700

    add more keymap

--- lua/vim-map.lua
+++ lua/vim-map.lua
@@ -12,10 +12,16 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", '"_dp')
 
 -- split
-vim.keymap.set("n", "<C-s>", ":vsplit<CR>")
+vim.keymap.set("n", "<C-BSlash>", ":vsplit<CR>")
 
 -- move between windows
 vim.keymap.set("n", "<S-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<S-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<S-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<S-l>", ":wincmd l<CR>")
+
+-- undo
+vim.keymap.set("n", "<C-z>", ":u<CR>")
+
+-- save
+vim.keymap.set("n", "<C-s>", ":w<CR>")
