684d98b513fa19dfd068d127c525e7061309ce9d
Truong Tien Dat <dd220302@gmail.com>
2025-08-25 15:42:55 +0700

    remove some lsp config

--- lsp/omnisharp.lua
+++
@@ -1,8 +0,0 @@
-return {
-	cmd = { "omnisharp" },
-	filetypes = { "cs", "cshtml" },
-	root_markers = { ".sln", ".csproj" },
-	handlers = {
-		["textDocument/definition"] = require("omnisharp_extended").handler,
-	},
-}
